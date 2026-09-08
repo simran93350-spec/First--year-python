56b44e1f774de2fb79c582809d2af97733763f47
simran93350-spec <simran93350@gmail.com>
2026-09-08 18:44:44 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,29 @@
 a = 10 
 b = 30
-print(a+b)
+sum = a + b
+print(sum)
+
+a = 20 
+b = 299
+sum = a - b
 print(a-b)
-print(a*b)
+
+a = 200
+b = 156
+sum = a * b 
+print(a *b)
+
+a = 72
+b = 2
+sum = a/b
 print(a/b)
+
+a = 23
+b = 10
+sum = a % b
 print(a%b)
+
+a = 23
+b = 22
+sum = a**b
+print(a**b)
