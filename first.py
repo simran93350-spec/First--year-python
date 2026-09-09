dfa414ea98b7201bf6bfe9de34f228f0ba85b91b
simran93350-spec <simran93350@gmail.com>
2026-09-09 21:54:31 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,18 @@
+name = "Simran Singh" 
+age = 19
+
+print("My name is :", name)
+print("My age is:", age)
+
+name = "Neetu Singh" 
+age  = 44
+
+print("My name is:", name)
+print("my age is:",age)
+
+name = "Ram Singh"
+age = 50
+age2 = age
+
+print( age2)
+
