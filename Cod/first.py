e2ac4843aa31a49783bea254f8000f7183a3219e
simran93350-spec <simran93350@gmail.com>
2026-09-10 21:51:29 +0530

    add initial files

---
+++ Cod/first.py
@@ -0,0 +1,40 @@
+
+name1 = 'Ritu'
+name2 = "Ragini"
+name3 = '''Yasmin'''
+
+print(name1)
+print(name2)
+print(name3)
+
+age = 23
+old = False
+a = None 
+print(type(old))
+print(type(a))
+
+age = 20
+old = True
+a = None
+print(type(old))
+print(type(a))
+
+# Relational Operators
+a = 50
+b = 20
+
+print(a==b)
+
+#Relational Operators
+a = 20
+b = 10
+
+print(a != b)
+
+#Relational Operators
+
+a = 30
+b = 30
+
+print(a==b)
+print(a != b)
